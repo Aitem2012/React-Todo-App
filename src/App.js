73ea21ee5ed9fec0260ff9fe282a5fbabb4c179e
Aitem2012 <ibrahim.ajumoko@decagon.dev>
2021-06-27 14:14:47 +0100

    Todo Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+    const [status, setStatus] = useState("all");
+    const [filteredTodos, setFilteredTodos] = useState([])
+
+    useEffect(() => {
+        getLocalTodos();
+    }, []);
+
+    useEffect(() => {
+        filterHandler()
+        saveLocalTodos()
+    }, [todos, status]);
+
+
+
+    const filterHandler = () => {
+        switch (status) {
+            case 'completed':
+                setFilteredTodos(todos.filter((todo) => todo.completed === true))
+                break;
+            case 'uncompleted':
+                setFilteredTodos(todos.filter((todo) => todo.completed !== true))
+                break;
+            default:
+                setFilteredTodos(todos)
+                break
+        }
+    }
+
+    const saveLocalTodos = () => {
+
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
+
+    const getLocalTodos = () => {
+        if (localStorage.getItem('todos') === null) {
+            localStorage.setItem('todos', JSON.stringify([]));
+        } else {
+            let localTodo = JSON.parse(localStorage.getItem('todos'))
+            setTodos(localTodo)
+        }
+    }
+
+
+    return ( <
+        div className = "App" >
+        <
+        header >
+        <
+        h1 > TechArt 's Todo Application</h1></header>  <
+        Form todos = { todos }
+        setStatus = { setStatus }
+        setTodos = { setTodos }
+        inputText = { inputText }
+        setInputText = { setInputText }
+        / > <
+        TodoList todos = { todos }
+        setTodos = { setTodos }
+        filteredTodos = { filteredTodos }
+        / > < /
+        div >
+    );
 }
 
 export default App;
